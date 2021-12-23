d2ef51e606e4000efd7b64e7d466d498572e76cf
Andyroze <11223344sasukeuchiha@gmial.com>
2021-12-24 04:24:30 +0545

    readfile

--- registration.js
+++ registration.js
@@ -1,7 +1,27 @@
 console.log("this is registration page")
 const registrationForm = document.getElementById("registration-form")
 console.log(registrationForm)
+
+function getSelected(select) {
+    let selectdValue = [];
+    for (optionIndex in select.options) {
+        if (select.options[optionIndex].selected) {
+            selectdValue.push(select.options[optionIndex].value);
+        }
+    }
+    console.log(selectedValue)
+    return selectdValue;
+
+}
+
 registrationForm.onsubmit = function(event) {
     event.preventDefault();
     console.log("Triggered");
+    let name = document.getElementById("NameInput").value;
+    let DOB = document.getElementById("DateOfBirthInput").value;
+    let darkMode = document.getElementById("DarkModeInput").checked;
+    let interests = getSelected(document.getElementById("InterestSelect"));
+
+    const data = { name, DOB, darkMode, interests }
+        // console.log(data)
 };
